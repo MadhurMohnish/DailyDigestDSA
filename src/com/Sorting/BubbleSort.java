8ac31c60b05407b713a10a401ab528bfc94cbcd7
Madhur Mohnish <Madhurmohnish@gmail.com>
2025-03-01 00:42:30 +0530

    Bubble Sort

--- src/com/Sorting/BubbleSort.java
+++ src/com/Sorting/BubbleSort.java
@@ -1,4 +1,4 @@
-package com;
+package com.Sorting;
 import java.util.Arrays;
 
 public class BubbleSort {
---
+++ src/com/Sorting/SelectionSort.java
@@ -0,0 +1,33 @@
+package com.Sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {2, -32, 0, 78, 1};
+        Selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static int Selection(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int last = arr.length - i - 1;
+            int maxIndex = getMaxIndex(arr, 0, last);
+            swap(arr, maxIndex, last);
+        }
+        return 0;
+    }
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+    static int getMaxIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end; i++) {
+            if (arr[max] < arr[i]) {
+                max = i;
+            }
+        }
+        return max;
+    }
+}
